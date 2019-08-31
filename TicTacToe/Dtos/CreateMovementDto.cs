e2495395c2c484302561ea46c92dfad1135b56b0
Fernando Souza <fernandosouza.eti@gmail.com>
2019-08-30 23:49:32 -0300

    Arquivos restantes do projeto web.
    Alguns arquivos não tinham sido enviados por ter sido utilizado o uploader web.

---
+++ TicTacToe/Dtos/CreateMovementDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicTacToe.Domain.Entities;
+
+namespace TicTacToe.Dtos
+{
+    public class CreateMovementDto
+    {
+        public Player Player { get; set; }
+        public PositionDto Position { get; set; }
+    }
+}
---
+++ TicTacToe/Dtos/PositionDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Dtos
+{
+    public class PositionDto
+    {
+        public byte X { get; set; }
+        public byte Y { get; set; }
+    }
+}
